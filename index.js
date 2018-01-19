08b68c147c1498c7cd1c287706879c02558863aa
Shannon Prescott <shan.prescott@outlook.com>
2018-01-19 19:30:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  
+  for (var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(array){
+  var count = 0;
+  var newArray = [];
+  
+  while(count < array.length){
+    newArray.push(`${array[count]}!!!`);
+    
+    count++;
+  }
+  
+  return newArray;
+}
+
+function iLoveTheBeatles(n){
+  var array = [];
+  
+  do {
+    array.push('I love the Beatles!');
+    n++;
+  } while (n < 15)
+  
+  return array;
